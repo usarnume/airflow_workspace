e094b570e0f139e1483b687c779233ffdf4ba480
usarnume <30601424+usarnume@users.noreply.github.com>
2023-10-02 13:11:51 +0000

    add python operator

--- dags/dag_1.py
+++ dags/dag_1.py
@@ -11,9 +11,12 @@ description='This DAG will print "Hello“ & "World".',
 schedule="@daily",
 ) as dag:
 
+    def _print_exec_date(**context):
+        print(f"This script was executed at {context['execution_date']}")
+
     procure_rocket_material = EmptyOperator(task_id="procure_rocket_material", dag=dag)
     build_stage_1 = BashOperator(task_id="build_stage_1", bash_command="echo '{{ task }} is running in the {{ dag }} pipeline'", dag=dag)
-    build_stage_2 = EmptyOperator(task_id="build_stage_2", dag=dag)
+    build_stage_2 = PythonOperator(task_id="build_stage_2", python_callable=_print_exec_date, provide_context=True, dag=dag)
     build_stage_3 = EmptyOperator(task_id="build_stage_3", dag=dag)
     procure_fuel = EmptyOperator(task_id="procure_fuel", dag=dag)
     launch = EmptyOperator(task_id="launch", dag=dag)
