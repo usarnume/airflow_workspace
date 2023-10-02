1d632396077bcf27ba8ed27cc3efa1b2655fdf58
usarnume <30601424+usarnume@users.noreply.github.com>
2023-10-02 13:05:07 +0000

    add bash operator

---
+++ dags/dag_1.py
@@ -0,0 +1,25 @@
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.models import DAG
+import pendulum
+
+with DAG(
+dag_id="hello_world",
+start_date = pendulum.today('UTC').add(days=-14),
+description='This DAG will print "Hello“ & "World".',
+schedule="@daily",
+) as dag:
+
+    procure_rocket_material = EmptyOperator(task_id="procure_rocket_material", dag=dag)
+    build_stage_1 = BashOperator(task_id="build_stage_1", bash_command="echo '{{ task }} is running in the {{ dag }} pipeline'", dag=dag)
+    build_stage_2 = EmptyOperator(task_id="build_stage_2", dag=dag)
+    build_stage_3 = EmptyOperator(task_id="build_stage_3", dag=dag)
+    procure_fuel = EmptyOperator(task_id="procure_fuel", dag=dag)
+    launch = EmptyOperator(task_id="launch", dag=dag)
+
+    procure_rocket_material >> [build_stage_1, build_stage_2, build_stage_3]
+    procure_fuel >> build_stage_3
+    [build_stage_1, build_stage_2, build_stage_3] >> launch
+
+
